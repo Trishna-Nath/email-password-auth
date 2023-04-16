5bcc7ef125dcf20de7dc5610bcc51b161cf36b7f
Trishna-Nath <t.nathbd@gmail.com>
2023-04-16 08:32:34 -0700

    added form

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+      console.log(email, password);
+    }
+    const handleEmailChange = (event) =>{
+    //   console.log(event.target.value);
+      setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = (event) => {
+        // console.log(event.target.value);
+    }
     return (
         <div>
 
 <h2>Please Register</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 
-                <input type="email" id="email" placeholder='your email' />
+                <input onChange={handleEmailChange} type="email" id="email" placeholder='your email' />
                 <br />
-                <input type="password" name='password' id='password' placeholder='your password' />
+                <input onBlur={handlePasswordBlur} type="password" name='password' id='password' placeholder='your password' />
                 <br />
-                <input type="submit" value="Register"/>
+                <input  type="submit" value="Register"/>
             </form>
         </div>
     );
